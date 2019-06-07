ba902364c06fd74b1c609b1aa643748ea75d66f9
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-06-07 19:51:50 +0200

    fighter create controller + route + view

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -6,4 +6,19 @@ class FightersController < ApplicationController
   def new
     @fighter = Fighter.new
   end
+
+  def create
+    @fighter = Fighter.new(fighter_params)
+    if @fighter.save
+      redirect_to fighters_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def fighter_params
+    params.require(:fighter).permit(:name, :health, :power)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :fighters, only: [:index, :new] do
+  resources :fighters, only: [:index, :new, :create] do
   end
 end
