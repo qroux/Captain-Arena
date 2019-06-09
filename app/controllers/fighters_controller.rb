a0660aee0a027e982762bdef54b94846378cd2d0
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-06-09 13:49:33 +0200

    fighter edit+update

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -17,6 +17,20 @@ class FightersController < ApplicationController
     end
   end
 
+  def edit
+    @fighter = Fighter.find(params[:id])
+  end
+
+  def update
+    @fighter = Fighter.find(params[:id])
+    @fighter.update(fighter_params)
+    if @fighter.save
+      redirect_to fighters_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def fighter_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :fighters, only: [:index, :new, :create] do
+  resources :fighters, only: [:index, :new, :create, :edit, :update] do
   end
 end
