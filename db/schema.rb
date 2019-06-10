d65b1e275d6e39d4f3e73969eb2c011b1a3c769d
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-06-10 19:45:46 +0200

    photo uploader cloudinary + carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_122830) do
+ActiveRecord::Schema.define(version: 2019_06_10_170727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_06_10_122830) do
     t.integer "power"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "fights", force: :cascade do |t|
