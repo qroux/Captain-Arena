acd7aa50e73f07606b57f757a60a31cb403cd935
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-06-10 12:53:49 +0200

    create fight controller

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -9,5 +9,20 @@ class FightsController < ApplicationController
   end
 
   def create
+    # @fight = Fight.new(fight_params) doesn't work, type mismatch, expect id, receive integer or string
+    @fight = Fight.new
+    @fight.player = Fighter.find_by(id: fight_params[:player])
+    @fight.opponent = Fighter.find_by(id: fight_params[:opponent])
+    if @fight.save
+      redirect_to fights_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def fight_params
+    params.require(:fight).permit(:player, :opponent)
   end
 end
