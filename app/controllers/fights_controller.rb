db68222611bcfc5e2f2d709a7f78ad3fefb27510
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-06-12 01:43:40 +0200

    fight#new fixed + fight validates added

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -33,6 +33,7 @@ class FightsController < ApplicationController
     # @fight = Fight.new(fight_params) doesn't work, type mismatch, expect id, receive integer or string
     @fight = Fight.new
     @fighters = Fighter.all #fix simple_form bug when render :new
+    @weapons = Weapon.all
     @fight.player = Fighter.find_by(id: fight_params[:player])
     @fight.player_weapon = Weapon.find_by(id: fight_params[:player_weapon])
     @fight.opponent = Fighter.find_by(id: fight_params[:opponent])
